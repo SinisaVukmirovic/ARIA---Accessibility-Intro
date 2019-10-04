6f713c3ef88ba6d075e52bc0d44f504dc1b09698
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2019-10-04 23:24:46 +0200

    Initial commit with basic setup

---
+++ script.js
@@ -0,0 +1,24 @@
+let accordions = document.querySelectorAll('.accordion');
+let expanders = document.querySelectorAll('.expander');
+
+accordions.forEach(accordion => {
+    accordion.addEventListener('click', function() {
+        let panel = this.nextElementSibling;
+
+        if (panel.style.display === 'block') {
+            panel.style.display = 'none';
+
+            // this.setAttribute('aria-expanded', 'false');
+
+            this.firstElementChild.classList.remove('up');
+            this.firstElementChild.classList.add('down');
+        }
+        else {
+            panel.style.display = 'block';
+
+            // this.setAttribute('aria-expanded', 'true');
+
+            this.firstElementChild.classList.add('up');
+        }
+    });
+});
